5fc25aa92291b2623dac77b66b7cf12f7d9e55a3
turbotyla <turbotylar@gmail.com>
2021-11-20 22:26:58 +1300

    updated misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -6,8 +6,36 @@ from youtube_search import YoutubeSearch
 import requests
 
 class Misc(commands.Cog, name="Miscellaneous"):
+    MOON_EMOJIS = {
+      "NEW_MOON": "🌑 New Moon",
+      "WAXING_CRESCENT": "🌒 Waxing Crescent",
+      "FIRST_QUARTER": "🌓 First Quarter",
+      "WAXING_GIBBOUS": "🌔 Waxing Bingus",
+      "FULL_MOON": "🌕 Full Moon",
+      "WANING_GIBBOUS": "🌖 Waning gibbous",
+      "LAST_QUARTER": "🌗 Last Quarter",
+      "WANING_CRESCENT": "🌘 Waning Crescent"
+    }
     def __init__(self, client):
         self.client = client
+    
+    @commands.command(
+        name="moon",
+        breif="Stats",
+        description="Get the current moon"
+        )
+    async def moon(self, ctx):
+        """ Returns the current moon, as an emoji. """
+        
+        t = datetime.datetime.now(pytz.timezone("utc"))
+        
+        mi = pylunar.MoonInfo((41, 16, 36), (174, 46, 40))
+        mi.update((t.year, t.month, t.day, t.hour, t.minute, t.second))
+
+        phase = mi.phase_name()
+        emoji = Stats.MOON_EMOJIS[phase]
+
+        await ctx.send(f"Current Moon: {emoji}")
 
     @commands.command(
         name="today",
